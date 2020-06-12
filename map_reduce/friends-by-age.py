8556cb6831d9c3887b9c643e2f34a4c5a63bae52
RubensSI <rubenssi825@gmail.com>
2020-06-11 22:51:39 -0300

    Add mrwordfraquency.py

--- map_reduce/friends-by-age.py
+++ map_reduce/friends-by-age.py
@@ -1,3 +1,15 @@
-from mrjob.job import MRJob
+from mrjob import MRJob
 
-class FriendsByAge(MRJob):
+class FriendByAge(mrjob):
+    def mapper(self, _, line):
+        (user, name, age, number) = line.split(',')
+
+        yield age, int(number)
+
+    def reducer(self, key, values):
+        itens = list(values)
+
+        yield key, sum(itens) / len(list)
+
+if __name__ == "__main__":
+    FriendByAge.run()
